53a7872b992de8705ad3a56892dc6f21202a8261
League <league@WTS8.attlocal.net>
2018-10-01 19:08:10 -0700

    perfect

--- src/Menu.java
+++ src/Menu.java
@@ -62,19 +62,18 @@ public class Menu implements ActionListener {
 			frame.pack();
 		}
 		if(combo.getSelectedItem().equals("Decimal") && combo2.getSelectedItem().equals("Binary")) {
-			label.setText(decToBin());
+			label.setText(decToBin(textBox.getText()));
 			frame.pack();
 		}
 		if(combo.getSelectedItem().equals("Decimal") && combo2.getSelectedItem().equals("Hexadecimal")) {		
-			label.setText(decToHex());
+			label.setText(decToHex(textBox.getText()));
 			frame.pack();
 		}
 		if(combo.getSelectedItem().equals("Binary") && combo2.getSelectedItem().equals("Decimal")) {
-			label.setText(binToDec());
+			label.setText(binToDec(textBox.getText()));
 			frame.pack();
 		}
-		if(combo.getSelectedItem().equals("Binary") && combo2.getSelectedItem().equals("Hexadecimal")) {
-
+		if(combo.getSelectedItem().equals("Binary") && combo2.getSelectedItem().equals("Hexadecimal")) { 
 			// Binary to hexadecimal
 			/* DO THIS: Bin -> Dec -> Hex */
 			try {
@@ -95,34 +94,22 @@ public class Menu implements ActionListener {
 			finally {
 				frame.pack();
 			}
-
+			label.setText(decToHex(binToDec(textBox.getText())));
+			frame.pack();
 		}
 		if(combo.getSelectedItem().equals("Hexadecimal") && combo2.getSelectedItem().equals("Binary")) {
-		    label.setText(hexToBin());
+		    label.setText(hexToBin(textBox.getText()));
 			frame.pack();
 		}
 		if(combo.getSelectedItem().equals("Hexadecimal") && combo2.getSelectedItem().equals("Decimal")) {
-			String digits = "0123456789ABCDEF";
-			
-			String hex = textBox.getText();
-			hex = hex.toUpperCase();
-			
-			int value = 0;
-			
-			for (int i = 0; i < hex.length(); i++) {
-				char c = hex.charAt(i);
-				int d = digits.indexOf(c);
-				value = 16 *  value + d;
-			}
-			
-			label.setText(""+value);
+			label.setText(""+hexToDec(textBox.getText()));
+			frame.pack();
 		}
 	}
 
-	public String decToBin() {
+	public String decToBin(String str) {
 		int[] binaryNum = new int[1000];
-		String s = textBox.getText();
-        	int n = Integer.parseInt(s);
+        	int n = Integer.parseInt(str);
         int i = 0;
         while (n > 0) 
         {
@@ -140,9 +127,8 @@ public class Menu implements ActionListener {
         }
 		return p;
 	}
-	public String decToHex() {
-		String s = textBox.getText();
-		int decimal = Integer.parseInt(s);
+	public String decToHex(String str) {
+		int decimal = Integer.parseInt(str);
 		int rem;  
 		String hex="";   
 		char hexNums[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};  
@@ -153,28 +139,26 @@ public class Menu implements ActionListener {
 		} 
 		return hex;
 	}
-	public String binToDec() {
-		String bin = textBox.getText();
-		bin = bin.trim();
+	public String binToDec(String str) {
+		str = str.trim();
 		int output = 0;
-		for (int i = 0; i < bin.length(); i++) {
-			if(bin.charAt(i) == '1') {
-				output += Math.pow(2, bin.length()-1-i);
+		for (int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == '1') {
+				output += Math.pow(2, str.length()-1-i);
 			}
 			
 		}
 		return output + "";
 	}
-	public String hexToBin() {
-		String hex = textBox.getText();
+	public String hexToBin(String str) {
 		String bin = "";
 	    String binFragment = "";
 	    int iHex;
-	    hex = hex.trim();
-	    hex = hex.replaceFirst("0x", "");
+	    str = str.trim();
+	    str = str.replaceFirst("0x", "");
 
-	    for(int i = 0; i < hex.length(); i++){
-	        iHex = Integer.parseInt(""+hex.charAt(i),16);
+	    for(int i = 0; i < str.length(); i++){
+	        iHex = Integer.parseInt(""+str.charAt(i),16);
 	        binFragment = Integer.toBinaryString(iHex);
 
 	        while(binFragment.length() < 4){
@@ -183,6 +167,19 @@ public class Menu implements ActionListener {
 	        bin += binFragment;
 	    }
 	    return bin;
+	}
+	public int hexToDec(String str) {
+		String s = textBox.getText();
+		String digits = "0123456789ABCDEF";
+        s = s.toUpperCase();
+        int val = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16*val + d;
+        }
+        return val;
+
 	}
 	
 }
